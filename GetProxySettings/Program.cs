9ca6b4a320269919427abf782734cf3e1e811df9
Brian Diggle <brian.diggle@callcredit.co.uk>
2014-05-07 15:59:41 +0100

    Initial commit

---
+++ GetProxySettings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace GetProxySettings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Net.IWebProxy webProxy = System.Net.WebProxy.GetDefaultProxy();
+
+            Uri requestaddress = new Uri("https://github.com");
+
+            Uri proxyAddress = webProxy.Get Proxy(requestaddress);
+
+            Console.WriteLine(proxyAddress.AbsoluteUri);
+
+            Console.ReadKey();
+        }
+    }
+}
